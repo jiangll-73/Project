e8fdb0826a8417b15e45bd337946bbe96b16a05c
jiangsyl <993456030@qq.com>
2020-10-17 22:46:46 +0800

    这是我的第一个新增文件

--- src/GitNewFile.java
+++ src/GitNewFile.java
@@ -3,5 +3,6 @@ public class GitNewFile {
         System.out.println("这是我新增的一个文件！");
         System.out.println("这是小明提交的代码！");
         System.out.println("这是我的提交");
+        System.out.println("这是我新功能修改的内容");
     }
 }
